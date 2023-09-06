daacef97e435e5537ebf6af8b3e3789a5bc48d2e
Beaprofessor <130960371+Beaprofessor@users.noreply.github.com>
2023-09-06 06:57:59 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,34 @@
+let score = "Lokesh"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber);
+// console.log(score);
+
+// console.log(ValueInNumber);
+
+
+// 33 => 33 
+// "33abc" => Nan (Not a Number)
+// null => 0
+// undefined => NaN
+// true => 1 ; false = 0 
+
+
+let isLoggedIn = "lokesh"
+
+// let BooleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanisLoggedIn);
+
+// 1 => true ; 0 => false ; "" => false ; "lokesh" => true;
+
+
+let someNumber = 22
+console.log(typeof someNumber);
+ 
+let StringNumber = String(someNumber)
+
+console.log(StringNumber);
+console.log(typeof StringNumber);
