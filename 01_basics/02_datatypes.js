e8771a72d754dec54c79ddc1ed810421a53eb12e
Beaprofessor <130960371+Beaprofessor@users.noreply.github.com>
2023-09-06 06:36:05 +0000

    Datatypes...

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // Treat all JS Code to newer version 
+
+// alert(1+1) // we are using nodejs , not browser 
+
+//console.log(3 
+  //  +23)   
+  
+ //console.log(3+23) console.log("heyy buddy")    
+
+ // code readability should be high mtlab easy to understand hona chahiye na ki chapriyo ki tarah likh diya......
+
+
+ let name = "Kartik"
+ let age = 22
+ let isLoggedIn = false
+
+/* Primitive Datatype */
+ // number datatype , range -> 2 ki power 53
+ // bigint
+ // string = ""
+ // boolean = true/false 
+ // null = standalone value
+ // undefined = abhi value assign ni hui hai 
+ // symbol = use for uniqueness 
+
+
+ // object 
+
+ console.log(typeof null); // object type 
+ console.log(typeof undefined); // undefined
+
